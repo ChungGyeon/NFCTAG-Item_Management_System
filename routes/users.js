242c23f61a6a608519a3bda3ad6dd335ef1d22a3
ChungGyeon <imsdm@naver.com>
2025-05-26 20:15:06 +0900

    feat: 로그인 곤련 세션 준완성 + 모듈 설치 완료
    로그인 안하고 페이지 접근시 세션 확인해서 안되어있으면 로그인 페이지로 안내, 로그인 후 세션 생성 후 정상접속 되도록 함
    추후 세션생성부분은 db랑 연결하고 손좀 봐야해

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,12 @@ router.post('/login', async (req, res) => {
 
   // 여기서 DB 확인 또는 검증 작업 수행
   if (studentnum === '2022039115' && password === '1234') {
+    if(!req.session.user) {
+      req.session.user = {
+        studentnum: studentnum,
+        password: password
+      };
+    }
     res.json({ success: true });
   } else {
     res.status(401).send('학번 또는 비밀번호가 틀렸습니다.');
