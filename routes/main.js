0fa6d31c5d56775c925b5220574d24d71dc96e4d
임재혁 <imsdm@naver.com>
2025-05-27 20:00:53 +0900

    merge: main브랜치 merge 완료

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const db = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,23 +10,6 @@ router.get('/', function(req, res, next) {
   //res.render('main', { title: 'ITS 물품대여소' });
   res.redirect('/LoadMysql');
 });
-
-router.get('/settingForItemList', function(req, res, next) {
-    if(!req.session.user){
-        return res.redirect('/users/login');
-    }
-    const { itemName, status, img} = null;
-
-    const sql = 'SELECT * FROM Items';
-    db.query(sql, (err, result) => {
-      if (err) {
-        console.error('아이템 리스트 불러오기 실패', err);
-        return res.status(500).send('아이템 리스트 불러오기 실패');
-      }
-      console.log('아이템 리스트 불러오기 성공:', result);
-      res.render('/',{itemName: itemName, status: status, img:img}); // 성공 후 관리자 페이지로 이동
-  });
-})
 
 
 router.get('/LoadMysql', (req, res) => {
