dcbfdb74c40f6f0ce80e439355c0a0c394033e91
임재혁 <imsdm@naver.com>
2025-06-25 18:00:06 +0900

    test: DB접근 실패시에도 테스트용 main에 접속할 수 있도록 설계

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const db = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
+const { db, testPageConnect } = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(!req.session.user){
-    return res.redirect('/users/login');
-  }
+    if(testPageConnect){
+         res.render('test_main');
+    }
+    if(!req.session.user){
+        return res.redirect('/users/login');
+     }
   //res.render('main', { title: 'ITS 물품대여소' });
   res.redirect('/LoadMysql');
 });
-
 
 router.get('/LoadMysql', (req, res) => {
     const sql = 'SELECT itemName, img FROM Items';
