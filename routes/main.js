a1df4efbf70b8740e1bfd98c4a102ff736d67e5f
acau7143 <sungmin7143@gmail.com>
2025-06-26 19:19:32 +0900

    feat 쿠키를 활용한 예약 및 취소 기능 구현

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// const db = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
 const { db, testPageConnect } = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
 
 /* GET home page. */
@@ -22,8 +21,47 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/reservation', (req, res) => {
+    const itemName = req.body.itemName;  // JSON.parse 제거
+    let currentReserved = req.cookies.reservedItems || '';
 
+    let reservedList = currentReserved
+        .split(',')
+        .filter(item => item);
 
+    if (reservedList.includes(itemName)) {
+        return res.send({ success: false, message: `${itemName}은(는) 이미 예약됨` });
+    }
+
+    reservedList.push(itemName);
+
+    res.cookie('reservedItems', reservedList.join(','), {
+        maxAge: 3600000,
+        httpOnly: false,
+        path: '/'
+    });
+
+    console.log('현재 예약 목록:', reservedList);
+    res.send({ success: true, message: `${itemName} 예약되었습니다.` });
+});
+
+router.post('/reservation/cancel', (req, res) => {
+    const itemName = req.body.itemName;
+    let currentReserved = req.cookies.reservedItems || '';
+
+    let reservedList = currentReserved
+        .split(',')
+        .filter(item => item && item !== itemName); // 해당 항목만 제거
+
+    res.cookie('reservedItems', reservedList.join(','), {
+        maxAge: 3600000,
+        httpOnly: false,
+        path: '/'
+    });
+
+    console.log(`${itemName} 예약 취소됨. 현재 목록:`, reservedList);
+    res.send({ success: true, message: `${itemName} 예약이 취소되었습니다.` });
+});
 
 //삭제예정
 router.get('/LoadMysql', (req, res) => {
