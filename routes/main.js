947ad19149f0eb45537f8e71c86696df53b06060
dong2357 <gd091330@gmail.com>
2025-07-14 19:08:50 +0900

    chore:주석추가
    123 주석 추가

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { db, testPageConnect } = require('./IMS_db'); //IMS_db.js에서 db 연결변수 가져오기
 
 /* GET home page. */
-router.get('/', function(req, res, next) {//router
+router.get('/', function(req, res, next) {//router123
     if (!req.session.user) {
         return res.redirect('/users/login');
     }
