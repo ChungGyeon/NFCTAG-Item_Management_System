741c2ae3d71d68d6da47a77c774f2e76d00fdba4
임재혁 <imsdm@naver.com>
2025-07-27 21:35:04 +0900

    docs: 아래 커밋의 연장선

--- public/javascripts/forUserMainPage.js
+++ public/javascripts/forUserMainPage.js
@@ -62,6 +62,8 @@ function cancelReservation(itemName) {
     }
 }
 
+
+//이건 이제 어디다 두지
 function cancelRental(itemName) {
     if (confirm(`${itemName} 대여를 취소하시겠습니까?`)) {
         fetch('/rent/cancel', {
@@ -80,29 +82,6 @@ function cancelRental(itemName) {
             .catch(err => {
                 alert("대여 취소 중 오류가 발생했습니다.");
                 console.error(err);
-            });
-    }
-}
-
-//detectCookies.js로 이동됨 추후 삭제 예정
-function confirmRental() {
-    if (confirm("예약한 물품을 대여 확정하시겠습니까?")) {
-        fetch('/rent/verify-step2', {
-            method: 'POST',
-            credentials: 'include'
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.success) {
-                    alert("✅ 대여 완료: " + data.message);
-                    location.reload();
-                } else {
-                    alert("❌ 실패: " + data.message);
-                }
-            })
-            .catch(err => {
-                alert("⚠️ 서버 오류 발생");
-                console.error('대여 요청 에러:', err);
             });
     }
 }
