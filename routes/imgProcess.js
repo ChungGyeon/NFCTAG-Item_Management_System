21afbf6952be198fbb08e7037e280f95d2781a4f
임재혁 <imsdm@naver.com>
2025-07-30 12:54:16 +0900

    build: imgProcess라우터에 fs모듈을 등록

--- routes/imgProcess.js
+++ routes/imgProcess.js
@@ -16,6 +16,7 @@ const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
 const path = require("path");
+const fs = require('fs');
 const { db }= require('./IMS_db'); // DB 연결
 
 
