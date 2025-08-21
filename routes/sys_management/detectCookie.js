4cce0abcd53fe4fdd4f6ef64d459c5d803ae3cdf
임재혁 <imsdm@naver.com>
2025-08-21 12:33:42 +0900

    chore: detectCookie 에러코드 변경
    그외에는 문제 없는 듯?

--- routes/sys_management/detectCookie.js
+++ routes/sys_management/detectCookie.js
@@ -48,7 +48,7 @@ router.get('/startRentingItem/:seed',(req,res) => {
 
     if (urlSeed === currentSeed || urlSeed === lastSeed) {
      if (!req.session.user) {
-         return res.status(404).send('로그인부터 하고 오십쇼');
+         return res.status(403).send('로그인부터 하고 오십쇼');
      }
      res.render('ckCookie',{title:"ITS-IMS : 쿠키 읽는 중"});
     } else {
